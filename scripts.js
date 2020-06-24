7ead8f0291ead8f3f682f163c7928da25867739c
JoHannah Joy <jojojoy2012@gmail.com>
2020-06-23 23:06:05 -0700

    add js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,21 @@
+let groupOutput = document.getElementById('group-output');
+let player = document.getElementById('player');
+let submit = document.getElementById('submit');
+
+let score = 0;
+// score = amount of submissions
+
 // if greg, onclick = greg.html as a table
+submit.addEventListener("click", function() {
+    if (player.value === 'bryan') {
+        console.log(player.value)
+        submit = bryan.html
+    } else if (name === 'david') {
+        submit = david.html
+    } else if (name === 'greg') {
+        submit = greg.html
+    } else if (name === 'steve') {
+        submit = steve.html
+    }
+    groupOutput.innerText = score++;
+})
